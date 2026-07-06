4ed23ed7efa453aa2a12c1851254f4e7213052f2
elishevarom <elishevarom@gmail.com>
2026-07-05 23:47:57 -0400

    add frontend tests

---
+++ frontend/src/components/navbar.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
+import Navbar from "./Navbar";
+
+test("shows navbar links", () => {
+  render(
+    <Authenticator.Provider>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    </Authenticator.Provider>
+  );
+
+  expect(screen.getByText(/home/i)).toBeInTheDocument();
+});
